56a19faf5dab92b4f2148ef6b6cca12d76d066bc
Vhisme <holuwathobilober@gmail.com>
2023-10-24 14:16:27 +0100

    update counter

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -4,21 +4,21 @@ import ErrorBoundary from "../ErrorBoundary";
 
 function Home() {
   return (
-    <>
+    <div className="home">
       <ErrorBoundary fallback>
         {" "}
-        
-        <h1>Welcome to our Counter Web...</h1>
+      
+        <h1>Altschool exam</h1>
         <p>
-          Please click out <Link to="/counter">counter app</Link>
+          <Link to="/counter">counter app</Link>
         </p>
         <ErrorTest />
         <p>
-          check out a 404 page here by clicking{" "}
+          {" "}
           <Link to="/">404 page</Link>
         </p>
       </ErrorBoundary>
-    </>
+    </div>
   );
 }
 export default Home;
